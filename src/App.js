7b38df3aa7b0d55d708540db224c9d41f23dc31a
thumbthing <ilscc0902@gmail.com>
2023-03-23 15:08:22 +0900

    immer 라이브러리

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import { useRef,useCallback, useState } from "react";
+import { useRef, useCallback, useState } from 'react';
+import produce from 'immer';
 
 const App = () => {
+
   const nextId = useRef(1);
-  const [form, setForm] = useState({ name: '', username: ''});
-  const [data, setData] = useState({
+  const [form, setForm] = useState({name: '', username: ''});
+  const [data ,setData] = useState({
     array: [],
     uselessValue: null
   });
 
-  const onChange = useCallback(
-    e => {
-      const { name, value } = e.target;
-      setForm({
-        ...form,
-        [name]: value
-      });
-    },
-    [form]
-  );
+  const onChange = useCallback( e => {
+    const { name, value } = e.target;
+    setForm(
+      produce(draft => {
+        draft[name] = value;
+      })
+    );
+  }, [])
 
   const onSubmit = useCallback(
     e => {
@@ -28,40 +28,43 @@ const App = () => {
         username: form.username
       };
 
-      setData( {
-        ...data,
-        array: data.array.concat(info)
-      } );
+      setData(
+        produce(draft => {
+          draft.array.push(info);
+        })
+      );
 
-      setForm( {
+      setForm({
         name: '',
         username: ''
-      } );
+      });
       nextId.current += 1;
     },
-    [data, form.name, form.username]
+    [form.name, form.username]
   );
 
   const onRemove = useCallback(
     id => {
-      setData({
-        ...data,
-        array: data.array.filter(info => info.id !== id)
-      });
+      setData(
+        produce(draft => {
+          draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
+        })
+      );
     },
-    [data]
+    []
   );
+
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input 
+        <input
           name="username"
           placeholder="아이디"
           value={form.username}
           onChange={onChange}
         />
-        <input 
+        <input
           name="name"
           placeholder="이름"
           value={form.name}
@@ -72,14 +75,14 @@ const App = () => {
       <div>
         <ul>
           {data.array.map(info => (
-            <li key={info.id} onClick={() => onRemove(info.id)}>
+            <li key={info.id} onClick={()=> onRemove(info.id)}>
               {info.username} ({info.name})
             </li>
           ))}
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
